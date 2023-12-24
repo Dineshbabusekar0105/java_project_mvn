df60098d501df138fc37a2d957856b22c6b69a5b
dinesh <dineshbabusekar0105@gmail.com>
2023-12-24 22:48:38 +0530

    simple java project

---
+++ mvn/word-length-calculator/README.md
@@ -0,0 +1,1 @@
+# This is a simple java project -in this project you can calculate the length of the given words  
---
+++ mvn/word-length-calculator/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+	Scanner scanner = new Scanner(System.in);
+        System.out.print( "enter the word:" );
+
+	String word = scanner.nextLine();
+
+	int length = word.length();
+
+	System.out.println("length of the entered word is:"+length);
+
+	scanner.close();
+    }
+}
